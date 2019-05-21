862879b2fca0c891ce7e47b68c802acc10d891b9
morganamaria25 <morgana.maria25@gmail.com>
2019-05-21 12:03:28 -0300

    Cadastro de autores funcionando

--- src/App.js
+++ src/App.js
@@ -1,28 +1,68 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import './css/pure-min.css';
 import './css/side-menu.css';
 import $ from 'jquery';
+import InputCustom from './componentes/InputCustom.js';
 
+
+// Guarda a lista do JSON
 class App extends Component {
-
   constructor() {
     super();
-    this.state = {lista : []};
+    this.state = {lista : [], nome:'', email:'', senha:''};
+    this.enviaForm = this.enviaForm.bind(this);
+    this.setNome = this.setNome.bind(this);
+    this.setEmail = this.setEmail.bind(this);
+    this.setSenha = this.setSenha.bind(this);
   }
 
-  componentWillMount(){
-    console.log("willMount");
+// O bind a cima é por que o this não está definido.
+
+// Setando o local do JSON
+  componentDidMount(){
+    // console.log("didMount");
     $.ajax({
         url:"http://localhost:8080/api/autores",
         dataType: 'json',
-        success:function(resposta){
-          console.log("chegou a resposta");
+        success:function(resposta){    
+          // console.log("chegou a resposta");          
           this.setState({lista:resposta});
-          }.bind(this)
+        }.bind(this)
+      } 
+    );          
+  }
+  // Evento do react
+  // O evento.preventDefault evita que a página seja recarregada em uma ação do botão
+  enviaForm(evento){
+    evento.preventDefault();
+    // Enviar os dados 
+    $.ajax({
+      url:"http://localhost:8080/api/autores",
+      contentType: 'application/json',
+      dataType: 'json',
+      type:'post',
+      // No metodo normal pegariamos o value do input. Por ser React utilizamos this.state. E depois declara no construtor onde guarda a lista com o JSON. Lembrar de incluir o .bind(this).
+      data: JSON.stringify({nome:this.state.nome,email:this.state.email,senha:this.state.senha}),
+      success: function(resposta){
+        this.setState({lista:resposta});
+      }.bind(this),
+      error: function(resposta){
+        console.log("Erro");
       }
-    );
+    });
   }
 
+  // Para pegar informação dos inputs
+
+  setNome(evento) {
+    this.setState({nome:evento.target.value});
+  }
+  setEmail(evento){
+    this.setState({email:evento.target.value});
+  }
+  setSenha(evento){
+    this.setState({senha:evento.target.value})
+  }
 
   render () {
     return (
@@ -46,56 +86,47 @@ class App extends Component {
         </div>
 
         <div id="main">
-            <div className="header">
-              <h1>Cadastro de Autores</h1>
-            </div>
-            <div className="content" id="content">
-              <div className="pure-form pure-form-aligned">
-                <form className="pure-form pure-form-aligned">
-                  <div className="pure-control-group">
-                    <label htmlFor="nome">Nome</label> 
-                    <input id="nome" type="text" name="nome" defaultValue=""  />                  
-                  </div>
-                  <div className="pure-control-group">
-                    <label htmlFor="email">Email</label> 
-                    <input id="email" type="email" name="email" defaultValue=""  />                  
-                  </div>
-                  <div className="pure-control-group">
-                    <label htmlFor="senha">Senha</label> 
-                    <input id="senha" type="password" name="senha"  />                                      
-                  </div>
-                  <div className="pure-control-group">                                  
-                    <label></label> 
-                    <button type="submit" className="pure-button pure-button-primary">Gravar</button>                                    
-                  </div>
-                </form>             
+          <div className="header">
+            <h1>Cadastro de Autores</h1>
+          </div>
+          <div className="content" id="content">
+            <div className="pure-form pure-form-aligned">
+              <form className="pure-form pure-form-aligned" onSubmit={this.enviaForm} method="post">
+                <InputCustom id="nome" type="text" name="nome" value={this.state.nome} onChange={this.setNome} label="Nome"/>                                              
+                <InputCustom id="email" type="email" name="email" value={this.state.email} onChange={this.setEmail} label="Email"/>                                              
+                <InputCustom id="senha" type="password" name="senha" value={this.state.senha} onChange={this.setSenha} label="Senha"/>                                                                       
+                <div className="pure-control-group">                                  
+                  <label></label> 
+                  <button type="submit" className="pure-button pure-button-primary">Gravar</button>                                    
+                </div>
+              </form>             
 
-              </div>  
-              <div>            
-                <table className="pure-table">
-                  <thead>
-                    <tr>
-                      <th>Nome</th>
-                      <th>email</th>
-                    </tr>
-                  </thead>
-                  <tbody>
-                    {
-                      this.state.lista.map(function(autor){
-                        return (
-                          <tr>
-                            <td>{autor.nome}</td>
-                            <td>{autor.email}</td>
-                          </tr>
-                        );
-                      })
-                    }
-                  </tbody>
-                </table> 
-              </div>             
-            </div>
-          </div> 
-    </div>
+            </div>  
+            <div>            
+              <table className="pure-table">
+                <thead>
+                  <tr>
+                    <th>Nome</th>
+                    <th>email</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  {
+                    this.state.lista.map(function(autor){
+                      return (
+                        <tr key={autor.id}>
+                          <td>{autor.nome}</td>
+                          <td>{autor.email}</td>
+                        </tr>
+                      );
+                    })
+                  }
+                </tbody>
+              </table> 
+            </div>             
+          </div>
+        </div>            
+      </div>     
     );
   }
 }
---
+++ src/componentes/InputCustom.js
@@ -0,0 +1,17 @@
+//  extends Component = Estou herdando um componente do react.
+
+import React, { Component } from 'react';
+
+export default class InputCustom extends Component{
+    
+    render() {
+        return(
+            <div className="pure-control-group">
+                <label htmlFor={this.props.id}>{this.props.label}</label>
+                <input id={this.props.id} type={this.props.type} name={this.props.name} value={this.props.value} onChange={this.props.onChange}/>
+            </div>          
+      );
+    }  
+}        
+
+
